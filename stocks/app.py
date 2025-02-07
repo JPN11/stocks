0395b95a2d63e2eda6089701c6ab40cbf471fc50
JPN11 <chaimeeina@gmail.com>
2025-02-07 20:44:16 +0000

    Refactor stock application routes and templates; update search and stock handling logic, improve error handling, and enhance UI elements.

--- stocks/app.py
+++ stocks/app.py
@@ -1,42 +1,49 @@
-from flask import Flask, render_template, request, jsonify
-import logging
+from flask import Flask, request, jsonify, render_template
 import os
+import logging
 from dotenv import load_dotenv
+import requests
 
-load_dotenv("static/api.env")
+load_dotenv("static/.env")
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
-
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    if request.method == "POST":
-        stock_name = request.form.get("symbol")
-    else:
-        stock_name = request.args.get("symbol")
-    if not stock_name:
-        return jsonify({"error": "Stock symbol is required"}), 400
-    return render_template("search.html", name=stock_name)
+    return render_template("search.html")
 
-@app.route("/stock", methods=["GET"])
+@app.route("/stock", methods=["GET", "POST"])
 def stock():
+    api_key = os.getenv("API_KEY")
+    if request.method == "POST":
+        stock = request.form.get("symbol")
+    elif request.method == "GET":
+        stock = request.args.get("symbol")
+    else:
+        return jsonify({"error": "Invalid request method"}), 405
+    if not stock:
+        return jsonify({"error": "No stock symbol provided"}), 400
+    api_url = f'https://api.api-ninjas.com/v1/stockprice?ticker={stock}'
     try:
-        stock_name = request.args.get("symbol")
-        if not stock_name:
-            return jsonify({"error": "Stock symbol is required"}), 400
-        api_key = os.getenv("API_KEY")
-        return render_template("stock.html", name=stock_name, api_key=api_key)
-    
-    except Exception as e:
-        logging.error(f"An error occurred: {e}", exc_info=True)
-        return jsonify({"error": "An error occurred"}), 500
+        response = requests.get(api_url, headers={'X-Api-Key': api_key})
+        response.raise_for_status()
+        data = response.json()
+        if "price" in data:
+            return render_template("stock.html", name=stock.upper(), price=usd(data["price"]))
+        else:
+            return jsonify({"error": "Failed to retrieve stock data"}), 500
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error: {e}")
+        return jsonify({"error": "Failed to retrieve stock data"}), 500
+
+
+def usd(value):
+    return f"${value:,.2f}"
 
 if __name__ == "__main__":
-    app.run(debug=os.environ.get("DEBUG", False))
+    app.run(debug=True)
